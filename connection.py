cc8191df852163d9288ea4712b92cb13e869cfc5
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-05-07 18:10:38 +0200

    upd

--- connection.py
+++ connection.py
@@ -1,13 +1,6 @@
 import psycopg2
 
-
 # Connect to the database
 def connect():
-    conn = psycopg2.connect(
-        database="postgres",
-        user="student",
-        password="HSDStoTestDb3711",
-        host="database-1.czcdhgn8biyx.us-east-1.rds.amazonaws.com",
-        port="5432"
-    )
+    conn = psycopg2.connect()
     return conn.cursor(), conn
