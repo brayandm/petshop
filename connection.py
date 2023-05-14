9c6043fd85566a9c3535bc3629cd4df028e85b6c
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-05-15 00:29:34 +0200

    install-redis

--- connection.py
+++ connection.py
@@ -1,21 +1,34 @@
 import psycopg2
 import os
+import redis
 from dotenv import load_dotenv
 
 
 # Connect to the database
-def connect():
+def connect_to_postgres():
     # Load the variables from the .env file
     load_dotenv()
 
     # Connect to the database
     conn = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        database=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        port=os.getenv('DB_PORT')
+        host=os.getenv('POSTGRES_HOST'),
+        database=os.getenv('POSTGRES_NAME'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        port=os.getenv('POSTGRES_PORT')
     )
 
     # Return the cursor (to execute the queries) and the connection (to commit the changes)
     return conn.cursor(), conn
+
+
+def connect_to_redis():
+    # Load the variables from the .env file
+    load_dotenv()
+
+    # Connect to the database
+    return redis.Redis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        password=os.getenv('REDIS_PASSWORD')
+    )
